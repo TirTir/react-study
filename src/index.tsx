1500d0d38ecab23ee5fe91664941896ca72c1b29
Junseo Oh <insung9546@gmail.com>
2023-01-24 05:39:39 +0900

    msw 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/servers/browser");
+  worker.start();
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
